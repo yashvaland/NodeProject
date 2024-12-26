657888cad15bef48c1ad9411f88e64c8a2a854f7
valand yash <yashvaland94@gmail.com>
2024-12-26 09:04:47 +0530

    update-1

---
+++ LOGIN SYSTEM WITH BLOG PROJECT/client/src/component/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-body-tertiary text-center text-lg-start">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }} 
+      >
+        © {new Date().getFullYear()} Copyright:
+        <a className="text-body" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
